3062b881cb341cae6c534e939da9663f38a27693
zuisong <com.me@foxmail.com>
2023-07-07 17:36:48 +0800

    use @happy-dom/global-registrator

--- test/common/dom-env-init.ts
+++ test/common/dom-env-init.ts
@@ -1,22 +1,12 @@
 import { delay } from "deno_std/async/delay.ts";
-import { GlobalWindow } from "npm:happy-dom@10";
+import { GlobalRegistrator } from "npm:@happy-dom/global-registrator@10";
 
 export async function init() {
-  const win = new GlobalWindow();
-  const doc = win.document;
-  doc.body.innerHTML = '<div class="container"></div>';
-
-  window.HTMLElement = win.HTMLElement as any;
-  window.HTMLImageElement = win.HTMLImageElement as any;
-  window.MutationObserver = win.MutationObserver as any;
-  window.document = doc as unknown as Document;
+  GlobalRegistrator.register();
+  document.body.innerHTML = '<div class="container"></div>';
   window.TextDecoder = undefined as any;
 }
 
 export async function tearDown() {
-  let id = window.setTimeout(function () {}, 0);
-  while (id--) {
-    window.clearTimeout(id); // will do nothing if no timeout with id is present
-  }
-  await delay(500);
+  await delay(1);
 }
