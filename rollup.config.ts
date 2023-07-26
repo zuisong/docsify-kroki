7d124e8d8ef84c5c785f67f10dca2fd87c7ac68b
zuisong <com.me@foxmail.com>
2023-07-26 13:46:40 +0800

    update rollup plugin

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,21 +1,48 @@
-import { swc } from "npm:rollup-plugin-swc3@0.9.1";
-import { rollup, RollupOptions } from "npm:rollup@3.26.3";
-import { terser } from "esm.sh/@wwa/rollup-plugin-terser@1.0.1?bundle";
+import {
+  rollup,
+  type RollupOptions,
+  type SourceMapInput,
+} from "esm.sh/rollup@3.26.3?bundle";
+import { transform } from "npm:@swc/wasm@1.3.70";
+import { minify } from "esm.sh/terser@5.19.2?bundle";
 const config: RollupOptions = {
-  input: "./src/index.ts",
+  input: { "docsify-kroki": "./src/index.ts" },
   output: {
     sourcemap: true,
     dir: "dist",
     format: "module",
   },
   plugins: [
-    swc({
-      sourceMaps: true,
-      env: {
-        targets: ["supports es6-module-dynamic-import"],
-      },
-    }),
-    terser({}),
+    {
+      name: "swc",
+      transform: (code) =>
+        transform(code, {
+          jsc: {
+            parser: {
+              syntax: "typescript",
+              decorators: true,
+            },
+          },
+          env: {
+            targets: ["supports es6-module-dynamic-import"],
+          },
+          sourceMaps: true,
+        }),
+    },
+    {
+      name: "terser",
+      renderChunk: (rawCode, _chunk, outputOptions) =>
+        minify(rawCode, {
+          sourceMap: outputOptions.sourcemap !== false,
+        })
+          .then((result) => ({
+            code: result.code!,
+            map: result.map as SourceMapInput,
+          })),
+    },
+  ],
+  external: [
+    "https://unpkg.com/compression-streams-polyfill@0.1.4/umd/index.js",
   ],
 };
 
