f1dd7b0b3674b71837b20becdd2fb7a2dcfb9ced
zuisong <com.me@foxmail.com>
2023-11-07 10:59:04 +0800

    upgrade deps

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export * as rollup from "npm:@rollup/wasm-node@4.1.6";
+export * as rollup from "rollup";
 export * as terser from "esm.sh/terser@5.21.0?bundle";
 export { default as resolveUri } from "esm.sh/@jridgewell/resolve-uri@3.1.1?bundle";
 export { default as fetchMock } from "esm.sh/fetch-mock@9.11.0/esm/client.js?target=denonext&bundle";
-export { GlobalRegistrator } from "npm:@happy-dom/global-registrator@12.9.1";
+export { GlobalRegistrator } from "@happy-dom/global-registrator";
