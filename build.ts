98a2d2d6af8186513adb9583024cf160a42f0a39
zuisong <com.me@foxmail.com>
2025-05-08 11:01:57 +0800

    upgrade deps
    Signed-off-by: zuisong <com.me@foxmail.com>

--- build.ts
+++ build.ts
@@ -1,4 +1,4 @@
-import * as esbuild from "npm:esbuild@0.25.0";
+import * as esbuild from "npm:esbuild@0.25.4";
 import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.11";
 import { emptyDir } from "jsr:@std/fs/empty-dir";
 import { resolve, toFileUrl } from "jsr:@std/path";
